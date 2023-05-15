98f93381880f2f8e7ebb3b2d1f17ce8918bdf6ed
shettychirag7 <shettychirag7@gmail.com>
2023-05-15 11:17:45 +0800

    test(profile): profile screen unit tests

--- src/screens/profile/__tests__/Profile.test.tsx
+++ src/screens/profile/__tests__/Profile.test.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import {fireEvent, render} from '@testing-library/react-native';
+
+import Profile from '../';
 
 describe('Profile screen tests', () => {
   it('should render correctly', () => {
-    expect(1).toEqual(1);
+    const {getByText} = render(
+      <Profile
+        navigation={
+          {
+            navigate: jest.fn(),
+          } as any
+        }
+      />,
+    );
+
+    expect(getByText('Go to notifications')).toBeDefined();
+  });
+
+  it('should navigate to notifications screen', () => {
+    const navigateMock = jest.fn();
+    const {getByText} = render(
+      <Profile
+        navigation={
+          {
+            navigate: navigateMock,
+          } as any
+        }
+      />,
+    );
+
+    fireEvent.press(getByText('Go to notifications'));
+
+    expect(navigateMock).toHaveBeenCalledWith('Notification');
   });
 });
